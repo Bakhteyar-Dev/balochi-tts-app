5c999e6d8fc69132f090b0cc9b96d5d1642d9e72
Bakhteyar Ghafoor <161243957+Bakhteyar-Dev@users.noreply.github.com>
2026-06-26 18:27:40 +0500

    Delete backend directory

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from api import views
-
-urlpatterns = [
-    path("health/", views.health, name="health"),
-    path("translate/", views.translate, name="translate"),
-    path("tts/", views.text_to_speech, name="tts"),
-]
