9a1305472b34bb3df04bf42e9732f78e0c72d5a4
Bakhteyar Ghafoor <161243957+Bakhteyar-Dev@users.noreply.github.com>
2026-06-27 02:38:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from bv_ui import (
     render_sidebar,
     render_topbar,
 )
-if "claude" in st.secrets:              
-    login(token=st.secrets["claude"])
+if "HF_TOKEN" in st.secrets:              
+    login(token=st.secrets["HF_TOKEN"])
 # ----------------------------------------------------------------------------
 # PAGE SETUP
 # ----------------------------------------------------------------------------
